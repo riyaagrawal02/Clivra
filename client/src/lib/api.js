28e7607704c1134af6268ffdd3a390bdea2e7e22
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2025-12-25 20:22:45 +0530

    reset project & initialize Clivra

--- client/src/lib/api.js
+++
@@ -1,7 +0,0 @@
-export async function apiRequest(path, options = {}) {
-  const res = await fetch(`http://localhost:5000/api${path}`, {
-    headers: { "Content-Type": "application/json" },
-    ...options,
-  });
-  return res.json();
-}
